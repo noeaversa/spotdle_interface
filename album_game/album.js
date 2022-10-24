0a5cd2d6f06f166ab8f18e8892d1463362668355
noeaversa <noeaversa@gmail.com>
2022-10-24 08:42:23 -0300

    lol

--- album_game/album.js
+++ album_game/album.js
@@ -1,3 +1,5 @@
+let lifeCounter = 1;
+
 function randomNumber(){
     return -(Math.floor(Math.random() * 225) + 1);
 }
@@ -12,8 +14,37 @@ function cofigurationImg(){
     image.style.marginTop = (margin_top + "px");
 }
 
+function changeLife(){
+    var imgRight = document.getElementById("heart-right");
+    var imgLeft = document.getElementById("heart-left");
+
+    if(lifeCounter == 1){
+        imgRight.src = "/spotdle_interface/images/cora_no_vida.png";
+        cofigurationImg();
+    }else if(lifeCounter == 0){
+        imgLeft.src = "/spotdle_interface/images/cora_no_vida.png";
+        restart();
+    }
+}
+
+function restart(){
+    window.alert("perdiste ESTUPIDO ESTUPIDO ESTUPIDO :)");
+}
+
+function verifyKey(){
+    let value_input = document.getElementById("input-search").value;
+    let key = "no se";
+
+    if(value_input != key){
+        changeLife();
+        lifeCounter--;
+    }
+    else
+        cofigurationImg();
+}
+
 function enterKeyPressed(event) {
     if (event.keyCode == 13) {
-        cofigurationImg();
+        verifyKey();
     }
  }
