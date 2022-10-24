8f0f200ee20d1733125624c37a254a2f1afe5167
noeaversa <noeaversa@gmail.com>
2022-10-24 09:59:54 -0300

    lol

--- album_game/album.js
+++ album_game/album.js
@@ -1,4 +1,6 @@
 let lifeCounter = 1;
+let puntos = 0;
+//let totalPointsUser = algo de la api;
 
 function randomNumber(){
     return -(Math.floor(Math.random() * 225) + 1);
@@ -9,6 +11,9 @@ function cofigurationImg(){
     let margin_top = randomNumber();
 
     let image = document.getElementById("album-img");
+
+    image.style.height = (500 + "px");
+    image.style.width = (500 + "px");
 
     image.style.marginLeft = (margin_left + "px");
     image.style.marginTop = (margin_top + "px");
@@ -27,7 +32,17 @@ function changeLife(){
     }
 }
 
+function correctAnswer(){
+    let image = document.getElementById("album-img");
+    
+    image.style.height = (275 + "px");
+    image.style.width = (275 + "px");
+    image.style.margin = 0 + "px";
+    puntos++;
+    document.getElementById("points").innerText = puntos;
 
+    
+}
 
 function verifyKey(){
     let value_input = document.getElementById("input-search").value;
@@ -37,17 +52,24 @@ function verifyKey(){
         changeLife();
         lifeCounter--;
     }
-    else
-        cofigurationImg();
+    else{
+        correctAnswer()
+        setTimeout(cofigurationImg, 1000);
+    }
 }
 
 function enterKeyPressed(event) {
     if (event.keyCode == 13) {
-        verifyKey();
+        verifyKey();  
     }
  }
 
  function restart(){
     window.alert("perdiste ESTUPIDO ESTUPIDO ESTUPIDO :)");
     lifeCounter = 1;
+    /*
+        if(totalPointsUser < puntos)
+            totalPointsUser = puntos;
+    */
+    puntos = 0;
 }
