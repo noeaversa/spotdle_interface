1746d10d5a8830e5d575fa2e5a360c695a1ce681
noeaversa <noeaversa@gmail.com>
2022-11-09 11:33:13 -0300

    lol

--- nav.js
+++ nav.js
@@ -3,20 +3,16 @@ function loadDataUser(){
         fetch("https://nigga.spotdle.ar/me/user/image", {
             credentials: "include",
         }).then(data => data.json()).then(data => {
-            console.log(data[0].url);
-            if(data[0].url != null || data[0].url != undefined || data[0].url != " "){
-                document.getElementById("user").src = data[0].url;
-                
-            }
+            if(data[0].url != null || data[0].url != undefined || data[0].url != " ")
+                document.getElementById("user").src = data[0].url; 
         });
+
         fetch("https://nigga.spotdle.ar/me/user/name", {
             credentials: "include"
         }).then(data => data.json()).then(data => {
             console.log(data);
-            /*
             if(data[0].url != null || data[0].url != undefined || data[0].url != " ")
                 document.getElementById("name-user").value = data; 
-            */
         });
     }
 }
