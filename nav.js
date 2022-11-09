d7a07f9cbdff044d05e6abd136160e28f5d5e42c
noeaversa <noeaversa@gmail.com>
2022-11-09 11:18:56 -0300

    lol

--- nav.js
+++ nav.js
@@ -16,9 +16,25 @@ function logOut(bool){
     if(document.cookie != null || document.cookie != undefined || document.cookie != " "){
         if(bool == true)
             window.alert("ATENCION, los datos del juegos no seran guardados ¿Esta seguro que quiere salir?");
-        let value_cookie = document.cookie;
-        document.cookie = value_cookie + '; Expires=Thu, 01 Jan 1970 00:00:01 GMT;'
+        clearCookies();
         document.getElementById("user").src = "/images/user.png";    
         window.location.reload(); 
+    }
+}
+
+function clearCookies() {
+    var cookies = document.cookie.split("; ");
+    for (var c = 0; c < cookies.length; c++) {
+        var d = window.location.hostname.split(".");
+        while (d.length > 0) {
+            var cookieBase = encodeURIComponent(cookies[c].split(";")[0].split("=")[0]) + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=' + d.join('.') + ' ;path=';
+            var p = location.pathname.split('/');
+            document.cookie = cookieBase + '/';
+            while (p.length > 0) {
+                document.cookie = cookieBase + p.join('/');
+                p.pop();
+            };
+            d.shift();
+        }
     }
 }
