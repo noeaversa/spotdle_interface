dd679fec3d36c6942d0132e99564877c0ab90c90
tomasbarak <barakcinthia@gmail.com>
2022-11-14 08:00:07 -0300

    lol

--- home/index.js
+++ home/index.js
@@ -2,7 +2,7 @@ function EnterAlbum(){
     fetch("https://nigga.spotdle.ar/me/user", {
             credentials: "include",
         }).then(data => data.json()).then(data => {
-            if(data.status === 200) {
+            if(data.status === 200 || data.status === undefined) {
                 window.location = "/album_game/album.html";
             } else {
                 window.alert("logueate primero");
