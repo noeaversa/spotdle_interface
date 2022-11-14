3ff6eafa95e6cf813936e21f2866694765b2176e
tomasbarak <barakcinthia@gmail.com>
2022-11-14 07:52:49 -0300

    lol

--- home/index.js
+++ home/index.js
@@ -2,7 +2,11 @@ function EnterAlbum(){
     fetch("https://nigga.spotdle.ar/me/user", {
             credentials: "include",
         }).then(data => data.json()).then(data => {
-            window.location = "/album_game/album.html";
+            if(data.status === 200) {
+                window.location = "/album_game/album.html";
+            } else {
+                window.alert("logueate primero");
+            }
         }).catch(error => {
             window.alert("logueate primero");
         });
